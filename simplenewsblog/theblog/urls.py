2444fb4247056a93141a70136d681d0d52d80d5c
zxbuzzy <zxbuzzy@gmail.com>
2021-07-20 14:37:30 +0300

    Базовая стилизация и навигация по блогу.

--- simplenewsblog/theblog/urls.py
+++ simplenewsblog/theblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from . import views
-from .views import HomeView
+from .views import HomeView, ArticleDetatilView
 
 urlpatterns = [
     # path('', views.home, name="home")
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name='home'),
+    path('article/<int:pk>', ArticleDetatilView.as_view(), name='article-detail'),
 ]
--- simplenewsblog/theblog/views.py
+++ simplenewsblog/theblog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    
+class ArticleDetatilView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
