d83239e81c19f9fcf5fc9f4b698cdf31a54c9629
zxbuzzy <zxbuzzy@gmail.com>
2021-07-21 11:23:29 +0300

    Добавлена возможность добавлять посты

--- simplenewsblog/theblog/urls.py
+++ simplenewsblog/theblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, ArticleDetatilView
+from .views import HomeView, ArticleDetatilView, AddPostView
 
 urlpatterns = [
     # path('', views.home, name="home")
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetatilView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add-post'),
 ]
