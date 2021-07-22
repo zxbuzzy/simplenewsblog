3fa0c76bd32336704c79844a5d9b25070cc0e8e1
zxbuzzy <zxbuzzy@gmail.com>
2021-07-22 19:49:07 +0300

    Стилизована страница для добавления поста

--- simplenewsblog/theblog/forms.py
+++ simplenewsblog/theblog/forms.py
@@ -1,2 +1,15 @@
 from django import forms
 from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'body')
+        
+        widgets = {
+            #Добавление класса form-control  
+			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название статьи'}),
+   			'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Тэг статьи'}),
+			'author': forms.Select(attrs={'class': 'form-control'}),
+   			'body': forms.Textarea(attrs={'class': 'form-control'}),
+		}
