22ef3b39a90a85dbd9435801e5443222cf2b4ba6
zxbuzzy <zxbuzzy@gmail.com>
2021-07-23 00:36:33 +0300

    Добавлена возможность регистрации и аутентификации

--- simplenewsblog/simplenewsblog/settings.py
+++ simplenewsblog/simplenewsblog/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'theblog'
+    'theblog',
+    'members'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
