7a181836fd73640627fe469e36ae327cb10d06b7
zxbuzzy <zxbuzzy@gmail.com>
2021-07-23 20:50:12 +0300

    Обновил static_root

--- simplenewsblog/simplenewsblog/settings.py
+++ simplenewsblog/simplenewsblog/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
