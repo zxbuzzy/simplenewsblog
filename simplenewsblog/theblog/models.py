a17d390597ec6f5188bf3bba1f5d9b442cecbe95
zxbuzzy <zxbuzzy@gmail.com>
2021-07-23 20:24:56 +0300

    Добавлена фотография профиля в навбар

--- simplenewsblog/theblog/models.py
+++ simplenewsblog/theblog/models.py
@@ -30,6 +30,11 @@ class Post(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_picture = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+    website_url = models.CharField(null=True, blank=True, max_length=255)
+    ig_url = models.CharField(null=True, blank=True, max_length=255)
+    twitter_url = models.CharField(null=True, blank=True, max_length=255)
+    vk_url = models.CharField(null=True, blank=True, max_length=255)
     
     def __str__(self):
         return str(self.user)
