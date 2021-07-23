85dc6361ee40273e7d2dbcdf26d1721c7ff686ab
zxbuzzy <zxbuzzy@gmail.com>
2021-07-23 03:35:33 +0300

    Добавлены категории

--- simplenewsblog/theblog/forms.py
+++ simplenewsblog/theblog/forms.py
@@ -1,27 +1,36 @@
+from typing import ItemsView
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for choice in choices:
+    choice_list.append(choice)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author',  'body', 'category')
         
         widgets = {
-            #Добавление класса form-control  
-			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название статьи'}),
-   			'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Тэг статьи'}),
+			'title': forms.TextInput(attrs={'class': 'form-control'}),
+   			'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
 			'author': forms.Select(attrs={'class': 'form-control'}),
+			'category': forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
    			'body': forms.Textarea(attrs={'class': 'form-control'}),
 		}
         
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag', 'body', 'category')
         
         widgets = {
             #Добавление класса form-control  
 			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название статьи'}),
    			'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Тэг статьи'}),
+            'category': forms.Select(choices=choice_list,attrs={'class': 'form-control'}),
    			'body': forms.Textarea(attrs={'class': 'form-control'}),
 		}
--- simplenewsblog/theblog/urls.py
+++ simplenewsblog/theblog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import HomeView, ArticleDetatilView, AddPostView, EditPostView, DeletePostView
+from .views import HomeView, ArticleDetatilView, AddPostView, EditPostView, DeletePostView, AddCategoryView
 
 urlpatterns = [
     # path('', views.home, name="home")
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArticleDetatilView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add-post'),
+    path('add_category/', AddCategoryView.as_view(), name='add-category'),
     path('article/edit/<int:pk>', EditPostView.as_view(), name='edit-post'),
     path('article/delete/<int:pk>', DeletePostView.as_view(), name='delete-post'),
 ]
